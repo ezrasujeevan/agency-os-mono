4c2effb842923b7653c889792355e3a530bb9115
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-05-10 15:45:00 +0530

    refactor: Add login, register, validate, and refresh endpoints to AuthClientController
    This commit adds the `login`, `register`, `validate`, and `refresh` endpoints to the `AuthClientController` in the `http-gateway` application. These endpoints allow clients to perform authentication-related actions such as logging in, registering, validating credentials, and refreshing tokens. This change enhances the functionality of the `AuthClientController` and improves the overall user experience.

--- apps/http-gateway/src/auth/client/auth.client.controller.ts
+++ apps/http-gateway/src/auth/client/auth.client.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthClientService } from './auth.client.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Client } from '@agency-os/class';
 
 @ApiTags('auth', 'client', 'auth/client', 'tcp/auth', 'tcp/auth/client')
 @Controller('auth/client')
 export class AuthClientController {
   constructor(private readonly authClientService: AuthClientService) {}
 
-  @Get()
-  async get() {
-    return 'hello From Auth Client Controller';
+  @Post('login')
+  async login(
+    @Body() login: Client.LoginClientRequestDto,
+  ): Promise<Client.LoginClientResponseDto> {
+    return await this.authClientService.loginClient(login);
+  }
+
+  @Post('register')
+  async register(
+    @Body() create: Client.CreateClientRequestDto,
+  ): Promise<Client.ClientResponseDto> {
+    return await this.authClientService.registerClient(create);
+  }
+
+  @Post('validate')
+  async validate(
+    @Body() validate: Client.ValidateClientRequestDto,
+  ): Promise<Client.ValidateClientResponseDto> {
+    return await this.authClientService.validateClient(validate);
+  }
+
+  @Post('refresh')
+  async refresh(
+    @Body() refresh: Client.RefreshTokenClientRequestDto,
+  ): Promise<Client.LoginClientResponseDto> {
+    return await this.authClientService.refreshTokenClient(refresh);
   }
 }
