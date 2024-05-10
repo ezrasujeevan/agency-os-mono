3905cbc0d93365b233429f6b6a8eabcf3136f4aa
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-05-10 13:34:20 +0530

    refactor: Update delivery entity and file entity relationship
    This commit refactors the `DeliveryEntity` and `DeliveryFileEntity` classes in the `tcp-delivery` application. It updates the relationship between the two entities by adding a `ManyToOne` association from `DeliveryFileEntity` to `DeliveryEntity`. This change allows for a more efficient and organized way of managing delivery files within the application.

--- apps/tcp-delivery/src/delivery/delivery.entity.ts
+++ apps/tcp-delivery/src/delivery/delivery.entity.ts
@@ -4,13 +4,14 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
 import { Delivery } from '@agency-os/class';
-import { DeliveryFileEntity } from './delivery.file.entity';
 
 @Entity('delivery')
 export class DeliveryEntity extends BaseEntity implements Delivery.Delivery {
@@ -56,4 +57,31 @@ export class DeliveryEntity extends BaseEntity implements Delivery.Delivery {
     { eager: true },
   )
   deliveryFiles: Delivery.DeliveryFile[];
+}
+
+@Entity('delivery_file')
+export class DeliveryFileEntity
+  extends BaseEntity
+  implements Delivery.DeliveryFile
+{
+  @PrimaryGeneratedColumn('uuid', { name: '_id' })
+  id: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+  @VersionColumn({ name: '_v' })
+  version: number;
+
+  @ManyToOne(() => DeliveryEntity, (delivery) => delivery.id)
+  @JoinColumn({ name: 'delivery_id' })
+  delivery: DeliveryEntity;
+
+  @Column({ name: 'file_version' })
+  fileVersion: string;
+
+  @Column('text', { name: 'file_url' })
+  fileUrl: string;
 }
