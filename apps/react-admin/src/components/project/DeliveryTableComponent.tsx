1e04b16d932e3a5b1204658ff8670fd993dc3def
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-05-12 00:40:26 +0530

    ```text refactor: Remove unused RowComponent and dummy.ts files, and add DeliveryTableComponent
    This commit removes the unused RowComponent.tsx and dummy.ts files from the react-admin application. These files are no longer needed and can be safely deleted. Additionally, it adds the DeliveryTableComponent.tsx file, which provides a table component for displaying delivery data. This new component enhances the functionality and user experience of the project page by allowing users to view and manage deliveries.

---
+++ apps/react-admin/src/components/project/DeliveryTableComponent.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Unstable_Grid2 as Grid, Skeleton } from '@mui/material'
+import { DataGrid, GridColDef, useGridApiRef } from '@mui/x-data-grid'
+import {
+    DeliveryRenderAccessCell,
+    DeliveryRenderActionCell,
+    DeliveryRenderFileUrlCell,
+    DeliveryRenderTagsCell
+} from './cell'
+
+import { dummyDelivery } from './dummy'
+import { Delivery } from '@agency-os/class'
+import { useGetAllDeliveryByProjectIdQuery } from '~/store/api'
+import { setTimeout } from 'timers/promises'
+import { flushSync } from 'react-dom'
+
+const deliveryColumns: GridColDef<Delivery.Delivery>[] = [
+    { field: 'deliverableName', headerName: 'Name' },
+    { field: 'deliverableType', headerName: 'Type' },
+    { field: 'description', headerName: 'Description' },
+    {
+        field: 'deliveryFilesVersion',
+        headerName: 'Version',
+        valueGetter: (value, { deliveryFiles }) =>
+            deliveryFiles.length > 0 ? deliveryFiles[0].version : 'N/A'
+    },
+    {
+        field: 'deliveryFilesUrl',
+        headerName: 'FileUrl',
+        renderCell: DeliveryRenderFileUrlCell
+    },
+    { field: 'tags', headerName: 'Tags', renderCell: DeliveryRenderTagsCell, sortable: false },
+    {
+        field: 'access',
+        headerName: 'Access',
+        description: 'Given access to Client.',
+        type: 'boolean',
+        editable: true,
+        renderCell: DeliveryRenderAccessCell
+    },
+    {
+        field: 'id',
+        headerName: 'Action',
+        renderCell: DeliveryRenderActionCell,
+        disableColumnMenu: true
+    }
+]
+
+interface DeliveryTableComponentProps {
+    projectId: string
+}
+
+const DeliveryTableComponent: React.FC<DeliveryTableComponentProps> = ({
+    projectId
+}: DeliveryTableComponentProps) => {
+    const apiRef = useGridApiRef()
+
+    const [rows, setRows] = useState<Delivery.Delivery[]>([])
+    const { data, isSuccess, isFetching } = useGetAllDeliveryByProjectIdQuery({ projectId })
+    useEffect(() => {
+        if (isSuccess) {
+            const { status, delivery }: Delivery.DeliveryResponseDto = data
+            if (status === 200 && delivery && Array.isArray(delivery)) {
+                setRows(delivery)
+                apiRef.current.autosizeColumns({ includeOutliers: true })
+            }
+        }
+    }, [data])
+
+    return (
+        <Grid container>
+            <Grid xs={12}>
+                <DataGrid
+                    apiRef={apiRef}
+                    columns={deliveryColumns}
+                    rows={rows}
+                    initialState={{ pagination: { paginationModel: { page: 0, pageSize: 5 } } }}
+                    pageSizeOptions={[5]}
+                    loading={isFetching}
+                    autosizeOptions={{ includeOutliers: true }}
+                    onResize={() => {
+                        apiRef.current.autosizeColumns({ includeOutliers: true })
+                    }}
+                />
+            </Grid>
+        </Grid>
+    )
+}
+
+export default DeliveryTableComponent
