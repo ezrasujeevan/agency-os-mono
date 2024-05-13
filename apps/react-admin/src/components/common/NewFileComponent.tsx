1c6b0763488c567426d2e3a91717a80efccb00ca
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-05-13 12:23:53 +0530

    ```text refactor: Add file upload functionality to NewFileComponent
    This commit adds file upload functionality to the NewFileComponent in the react-admin application. It introduces a file input field, handles file selection and change events, and includes a submit handler to handle form submission. The selected file is stored in the component state using the useState hook. This enhancement allows users to upload files and perform necessary actions, such as saving or updating a file.

--- apps/react-admin/src/components/common/NewFileComponent.tsx
+++ apps/react-admin/src/components/common/NewFileComponent.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Unstable_Grid2 as Grid } from '@mui/material'
+import React, { useState } from 'react'
+import { Button, Unstable_Grid2 as Grid, TextField, VisuallyHiddenInput } from '@mui/material'
+import { CloudUpload } from '@mui/icons-material'
 
 interface NewFileComponentProps {
-    projectId:string
+    projectId: string
     deliveryId?: string
     assetId?: string
 }
@@ -11,10 +12,41 @@ const NewFileComponent: React.FC<NewFileComponentProps> = ({
     assetId,
     deliveryId
 }: NewFileComponentProps) => {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null)
+    // const [upload, { isLoading, error }] = useUploadMutation()
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedFile(event.target.files?.[0] ?? null)
+    }
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        // if (selectedFile) {
+        //     try {
+        //         await upload(selectedFile)
+        //         // Handle successful upload (e.g., display success message)
+        //     } catch (error) {
+        //         console.error('Upload failed:', error)
+        //     }
+        // }
+    }
+
     return (
-        <Grid container>
-            <Grid xs={12}>NewFileComponent</Grid>
-        </Grid>
+        <form onSubmit={handleSubmit}>
+            <TextField
+                type="file"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                label="Select File"
+                onChange={handleFileChange}
+                // error={error}
+                // helperText={error?.data?.message} // Display error message from API response (if any)
+            />
+            <Button variant="contained" type="submit" disabled={isLoading}>
+                {'Upload'}
+            </Button>
+        </form>
     )
 }
 
