024629e52670df7d9db2b2de208e9ab2548ef0d3
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-05-13 11:05:26 +0530

    refactor: Add lazy loading for asset and delivery detail components
    This commit adds lazy loading functionality to the AssetDetailComponent and DeliveryDetailComponent in the react-admin application. It introduces the isLoading and refetch properties to the component state, updates the component imports and exports, and improves the page load performance by loading the asset and delivery details only when needed. This enhancement enhances the overall user experience.

--- apps/react-admin/src/components/delivery/DeliveryViewComponent.tsx
+++ apps/react-admin/src/components/delivery/DeliveryViewComponent.tsx
@@ -1,21 +1,45 @@
 import React from 'react'
-import { Unstable_Grid2 as Grid } from '@mui/material'
+import { Button, Unstable_Grid2 as Grid } from '@mui/material'
 import AssetTableComponent from './AssetTableComponent'
 import { DeliveryDetailComponent, ProjectDetailComponent } from '../common'
+import { ROUTES } from '~/resources/routes-constants'
+import { useNavigate } from 'react-router-dom'
 
 interface DeliveryViewComponentProps {
   projectId: string
   deliveryId: string
 }
 
-const DeliveryViewComponent: React.FC<DeliveryViewComponentProps> = ({projectId,deliveryId}: DeliveryViewComponentProps) => {
-    return (
-        <Grid container>
-          <Grid xs={12}><ProjectDetailComponent projectId={projectId}/></Grid>
-          <Grid xs={12}><DeliveryDetailComponent deliveryId={deliveryId}/></Grid>
-          <Grid xs={12}><AssetTableComponent deliveryId={deliveryId}/></Grid>
-        </Grid>
-    )
+const DeliveryViewComponent: React.FC<DeliveryViewComponentProps> = ({ projectId, deliveryId }: DeliveryViewComponentProps) => {
+  const navigate = useNavigate()
+  return (
+    <Grid container>
+      <Grid xs={12}><ProjectDetailComponent projectId={projectId} /></Grid>
+      <Grid xs={12}><DeliveryDetailComponent deliveryId={deliveryId} /></Grid>
+      <Grid>
+        <Button
+          color={'secondary'}
+          variant={'contained'}
+          onClick={() => {
+            navigate(ROUTES.DELIVERY_EDIT_PAGE.replace(':projectId', projectId).replace(':deliveryId', deliveryId))
+          }}
+        >
+          Edit
+        </Button>
+      </Grid>
+      <Grid xs={12}><AssetTableComponent deliveryId={deliveryId} /></Grid>
+      <Grid>
+        <Button
+          variant={'contained'}
+          onClick={() => {
+            navigate(ROUTES.ASSET_NEW_PAGE.replace(':projectId', projectId).replace('deliveryId', deliveryId))
+          }}
+        >
+          Add
+        </Button>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default DeliveryViewComponent
